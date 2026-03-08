4f8a420737e4f7e70be7dae8463235ece54ed7fd
SamuelJ11 <sjab225@uky.edu>
2026-03-08 13:03:03 +0000

    minor formatting changes

--- Project2/CalcTimeIntval.c
+++ Project2/CalcTimeIntval.c
@@ -14,13 +14,13 @@ typedef struct {
 void TimeInterval(calcTimeIntval *packetdata, struct timeval *start, struct timeval *end) 
 {
     /* SampleRTT is cacluated calculated using the gettimeofday() function and the timeval struct */
-    packetdata->SampleRTT = (end->tv_sec - start->tv_sec) * 1000000 + (end->tv_usec - start->tv_usec);
+    packetdata->SampleRTT = (end->tv_sec - start->tv_sec)*1000000 + (end->tv_usec - start->tv_usec);
 
     /* Upon obtaining a new SampleRTT, we update EstimatedRTT according to the formula below */
     packetdata->EstimatedRTT = (1 - packetdata->alpha)* packetdata->EstimatedRTT + packetdata->alpha*(packetdata->SampleRTT);
 
     /* We calculate DevRTT be an estimate of how much Sample RTT typically deviates from EstimatedRTT */
-    packetdata->DevRTT = (1 - packetdata->beta)* packetdata->DevRTT + packetdata->beta*fabs( packetdata->SampleRTT - packetdata->EstimatedRTT);
+    packetdata->DevRTT = (1 - packetdata->beta)* packetdata->DevRTT + packetdata->beta*fabs(packetdata->SampleRTT - packetdata->EstimatedRTT);
 
     /* Finally, obtain use the formula for retransmission timeout interval as a function of EstimatedRTT and DevRTT */
     packetdata->TimeoutInterval = packetdata->EstimatedRTT + 4*(packetdata->DevRTT);
