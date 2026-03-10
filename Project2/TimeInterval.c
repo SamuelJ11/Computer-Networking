bb8c45d13af35e5ee96dce07111194ab9255732b
SamuelJ11 <sjab225@uky.edu>
2026-03-10 09:33:35 +0000

    Restructured, added new members

--- Project2/TimeInterval.c
+++ Project2/TimeInterval.c
@@ -1,10 +1,14 @@
 #include <math.h>
 #include "TimeInterval.h"
 
-void TimeInterval(calcTimeIntval *packetdata, long long *start, long long *end) 
+void TimeInterval(calcTimeIntval *packetdata) 
 {
+    /* Extract start and end times from the nested timespec structs that were populated by clock_gettime() */
+    long start = (packetdata->start.tv_sec*1000000) + (packetdata->start.tv_nsec/1000); /* start time in µs */
+    long end = (packetdata->end.tv_sec*1000000) + (packetdata->end.tv_nsec/1000); /* end time in µs */
+
     /* SampleRTT is cacluated calculated using the gettimeofday() function and the timeval struct */
-    packetdata->SampleRTT = *end - *start;
+    packetdata->SampleRTT = end - start;
 
     /* Upon obtaining a new SampleRTT, we update EstimatedRTT according to the formula below */
     packetdata->EstimatedRTT = (1 - packetdata->alpha)* packetdata->EstimatedRTT + packetdata->alpha*(packetdata->SampleRTT);
@@ -13,5 +17,6 @@ void TimeInterval(calcTimeIntval *packetdata, long long *start, long long *end)
     packetdata->DevRTT = (1 - packetdata->beta)* packetdata->DevRTT + packetdata->beta*fabs(packetdata->SampleRTT - packetdata->EstimatedRTT);
 
     /* Finally, obtain use the formula for retransmission timeout interval as a function of EstimatedRTT and DevRTT */
-    packetdata->TimeoutInterval = packetdata->EstimatedRTT + 4*(packetdata->DevRTT);
+    packetdata->TimeoutInterval.tv_sec = (packetdata->EstimatedRTT + 4*(packetdata->DevRTT)) / 1000000; /* convert µs to s */
+    packetdata->TimeoutInterval.tv_nsec = (packetdata->EstimatedRTT + 4*(packetdata->DevRTT)) * 1000; /* convert µs to ns */
 }
--- Project2/TimeInterval.h
+++ Project2/TimeInterval.h
@@ -1,15 +1,19 @@
 #ifndef TIMEINTERVAL_H
 #define TIMEINTERVAL_H
 
+#include <time.h>
+
 typedef struct {
+    struct timespec start;
+    struct timespec end;
     double SampleRTT;
     double EstimatedRTT;
     double DevRTT;
     double alpha;
     double beta;
-    double TimeoutInterval;
+    struct timespec TimeoutInterval; /* this needs to be a struct for epoll_pwait2() */
 } calcTimeIntval;
 
-void TimeInterval(calcTimeIntval *packetdata, long long *start, long long *end);
+void TimeInterval(calcTimeIntval *packetdata);
 
 #endif
