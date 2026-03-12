b76a0badc403e2e1d558bb1aa9b417bd1342e1b3
SamuelJ11 <sjab225@uky.edu>
2026-03-12 10:00:47 +0000

    Added 2 separate functions for client and server

--- Project2/Packet.c
+++ Project2/Packet.c
@@ -1,112 +1,44 @@
 #include "Packet.h"
+#include <string.h>
 
-char *Serialize(unsigned char flag, packet_struct packet) 
+void SerializeClient(client_struct *packet, char *buf)
 {
-    /* This byte will be used to indicate whether the packet is from the client or server */
-    unsigned char flag = flag; 
+    /* At buffer offset 0, we store the first field (seq_num) which has a size of 2 bytes */
+    memcpy(buf, &packet->seq_num, sizeof(packet->seq_num));
 
-    switch (flag)
-    {
-        case 0: /* packet is from client */
+    /* At buffer offset 2, we store the beggining of the message, which has a size of 16 bytes */
+    memcpy(buf + sizeof(packet->seq_num), packet->message, MESSAGE_SIZE);
 
-            /* Calculate the size of the message */
-            int message_size = strlen(packet.message);  
-
-            /* Calculate the total buffer size for the client */
-            int buffer_size = sizeof(packet.seq_num) + message_size + sizeof(client_buf); /* 2000 + 32 = 2032 bytes */
-
-            /* Allocate buffer_size + 1 bytes */
-            char *buf = buffer_size + 1;  /* allocate 2033 bytes, the extra byte is for null-termination */
-
-            /* At buffer offset 0, we store the first field (seq_num) which has a size of 2000 bytes */
-            memcpy(buf, &packet.seq_num, sizeof(packet.seq_num));
-
-            /* At buffer offset 2000, we store the beggining of the message, which has a size of 16 bytes */
-            memcpy(buf + sizeof(packet.seq_num), &packet.message, message_size);
-
-            /* At buffer offset 2016, we store the beggining of the client buffer, which has a size of 16 bytes */
-            memcpy(buf + sizeof(packet.seq_num) + message_size, &packet.client_buf, message_size);
-
-            /* At buffer offset 2032, we can optionally store a null terminator to allow printing of the message */
-            buf[buffer_size] = '\0';
-
-            return buf;
-            break;
-
-        case 1: /* packet is from server */
-            
-            /* Calculate the size of the message */
-            int message_buf = strlen(packet.echo_buf);  
-
-            /* Calculate the total buffer size for the server */
-            int buffer_size = sizeof(packet.ack_num) + message_buf; /* 2000 + 16 = 2016 bytes */
-
-            /* Allocate buffer_size + 1 bytes */
-            char *buf = buffer_size + 1;  /* allocate 2017 bytes, the extra byte is for null-termination */
-
-            /* At buffer offset 0, we store the first field (seq_num) which has a size of 2000 bytes */
-            memcpy(buf, &packet.ack_num, sizeof(packet.ack_num));
-
-            /* At buffer offset 2000, we store the beggining of the echo buffer, which has a size of 16 bytes */
-            memcpy(buf + sizeof(packet.ack_num), &packet.echo_buf, message_buf);
-
-            /* At buffer offset 2016, we can optionally store a null terminator to allow printing of the message */
-            buf[buffer_size] = '\0';
-
-            return buf;
-            break;
-
-        default:
-            /* Invalid flag recieved */
-            DieWithError("invalid flag for serialization");
-    }
+     /* At buffer offset 18, we store the beggining of the client buffer, which has a size of 16 bytes */
+    memcpy(buf + sizeof(packet->seq_num) + MESSAGE_SIZE, packet->client_buf, MESSAGE_SIZE);
 }
 
-char *Deserialize(unsigned char flag, char *buf)
+void DeserializeClient(char *buf, client_struct *packet)
 {
-    /* This byte will be used to indicate whether the packet is from the client or server */
-    unsigned char flag = flag; 
+    /* At buffer offset 0, we read the first field (seq_num) which has a size of 2 bytes */
+    memcpy(&packet->seq_num, buf, sizeof(packet->seq_num));
 
-    switch (flag)
-    {
-        case 0: /* packet is from client */
+    /* At buffer offset 2, we read the second field (message) which has a size of 16 bytes */
+    memcpy(packet->message, buf + sizeof(packet->seq_num), MESSAGE_SIZE);
 
-            /* Declare a client_struct variable to hold the deserialized data */
-            client_struct client_packet;
+    /* At buffer offset 18, we read the third field (client_buf) which has a size of 16 bytes */
+    memcpy(packet->client_buf, buf + sizeof(packet->seq_num) + MESSAGE_SIZE, MESSAGE_SIZE);
+}
 
-            /* Calculate the size of the message */
-            int message_size = strlen(packet.message); 
+void SerializeServer(server_struct *packet, char *buf)
+{
+    /* At buffer offset 0, we store the first field (seq_num) which has a size of 2 bytes */
+    memcpy(buf, &packet->expected_seqnum, sizeof(packet->expected_seqnum));
 
-            /* At buffer offset 0, we read the first field (seq_num) which has a size of 2000 bytes */
-            memcpy(&client_packet.seq_num, buf, sizeof(client_packet.seq_num));
+    /* At buffer offset 2, we store the beggining of the echo buffer, which has a size of 16 bytes */
+    memcpy(buf + sizeof(packet->expected_seqnum), packet->echo_buf, MESSAGE_SIZE);
+}
 
-            /* At buffer offset 2000, we read the second field (message) which has a size of 16 bytes */
-            memcpy(&client_packet.message, buf + sizeof(client_packet.seq_num), message_size);
+void DeserializeServer(char *buf, server_struct *packet)
+{
+    /* At buffer offset 0, we read the first field (expected_seqnum) which has a size of 2 bytes */
+    memcpy(&packet->expected_seqnum, buf, sizeof(packet->expected_seqnum));
 
-            /* At buffer offset 2016, we read the third field (client_buf) which has a size of 16 bytes */
-            memcpy(&client_packet.client_buf, buf + sizeof(client_packet.seq_num) + message_size, sizeof(client_buf));
-
-            /* Return the reconstructed client packet struct */
-            return client_packet;
-            break;
-
-        case 1: /* packet is from server */
-
-            /* Declare a server_struct variable to hold the deserialized data */
-            server_struct server_packet;
-
-            /* At buffer offset 0, we read the first field (ack_num) which has a size of 2000 bytes */
-            memcpy(&server_packet.ack_num, buf, sizeof(server_packet.ack_num));
-
-            /* At buffer offset 2000, we read the second field (echo_buf) which has a size of 16 bytes */
-            memcpy(&server_packet.echo_buf, buf + sizeof(server_packet.ack_num), sizeof(echo_buf));
-
-            /* Return the reconstructed server packet struct */
-            return server_packet;
-            break;
-
-        default:
-            /* Invalid flag recieved */
-            DieWithError("invalid flag for deserialization");
-    }
+    /* At buffer offset 2, we read the second field (echo_buf) which has a size of 16 bytes */
+    memcpy(packet->echo_buf, buf + sizeof(packet->expected_seqnum), MESSAGE_SIZE);
 }
--- Project2/Packet.h
+++ Project2/Packet.h
@@ -5,17 +5,20 @@
 
 /* Initialize the struct members with a fixed message size and sequence number array */
 typedef struct {
-    unsigned short seq_num[1000]; /* used to hold sequence number of each packet */
-    char message[16] = "Hello, Shixiong!"; /* send 16 bytes every time */
-    char client_buf[16]; /* used to hold the message recieved by the client */
+    unsigned short seq_num; /* used to hold sequence number of each packet */
+    char message[MESSAGE_SIZE]; /* send 16 bytes every time */
+    char client_buf[MESSAGE_SIZE]; /* used to hold the message recieved by the client */
 } client_struct;
 
 typedef struct {
-    unsigned short ack_num[1000]; /* used to hold ack number of each packet */
-    char echo_buf[16]; /* used to hold the message echoed back by the server */
+    unsigned short expected_seqnum; /* used to hold ack number of the last correctly recieved packet */
+    char echo_buf[MESSAGE_SIZE]; /* used to hold the message echoed back by the server */
 } server_struct;
 
-char *Serialize(unsigned char flag, packet_struct packet); /* serialize the packet struct into a byte stream to send over the network */
-char *Deserialize(unsigned char flag, char *buf); /* takes the serialized buffer we generated and reconstructs the original packet struct */
+void SerializeClient(client_struct *packet, char *buf);  /* serialize the client packet struct into a byte stream to send over the network */
+void DeserializeClient(char *buf, client_struct *packet); /* takes the serialized buffer we generated and reconstructs the original packet struct */
+
+void SerializeServer(server_struct *packet, char *buf);  /* serialize the server packet struct into a byte stream to send over the network */
+void DeserializeServer(char *buf, server_struct *packet); /* takes the serialized buffer we generated and reconstructs the original packet struct */
 
 #endif
