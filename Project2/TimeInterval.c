57794d64ec4a4ad6407c7d11dd9f30106eb27091
SamuelJ11 <sjab225@uky.edu>
2026-03-13 04:49:34 +0000

    General code cleaning

--- Project2/TimeInterval.c
+++ Project2/TimeInterval.c
@@ -1,25 +1,25 @@
 #include <math.h>
 #include "TimeInterval.h"
 
-void TimeInterval(calcTimeIntval *packetdata) 
+void TimeInterval(calcTimeIntval *pt) 
 {
     /* Extract start and end times from the nested timespec structs that were populated by clock_gettime() */
-    long start = (packetdata->start.tv_sec*1000000) + (packetdata->start.tv_nsec/1000); /* start time in µs */
-    long end = (packetdata->end.tv_sec*1000000) + (packetdata->end.tv_nsec/1000); /* end time in µs */
+    long start = (pt->start.tv_sec*1000000) + (pt->start.tv_nsec/1000); /* start time in µs */
+    long end = (pt->end.tv_sec*1000000) + (pt->end.tv_nsec/1000); /* end time in µs */
 
     /* SampleRTT is cacluated calculated using the gettimeofday() function and the timeval struct */
-    packetdata->SampleRTT = end - start;
+    pt->SampleRTT = end - start;
 
     /* Upon obtaining a new SampleRTT, we update EstimatedRTT according to the formula below */
-    packetdata->EstimatedRTT = (1 - packetdata->alpha)* packetdata->EstimatedRTT + packetdata->alpha*(packetdata->SampleRTT);
+    pt->EstimatedRTT = (1 - pt->alpha) * pt->EstimatedRTT + pt->alpha*(pt->SampleRTT);
 
     /* We calculate DevRTT be an estimate of how much Sample RTT typically deviates from EstimatedRTT */
-    packetdata->DevRTT = (1 - packetdata->beta)* packetdata->DevRTT + packetdata->beta*fabs(packetdata->SampleRTT - packetdata->EstimatedRTT);
+    pt->DevRTT = (1 - pt->beta) * pt->DevRTT + pt->beta*fabs(pt->SampleRTT - pt->EstimatedRTT);
 
     /* Compute the round trip time in microsends before populating the timespec struct*/
-    long rto_µs = packetdata->EstimatedRTT + 4*(packetdata->DevRTT); /* time in µs */
+    long rto_µs = pt->EstimatedRTT + 4*(pt->DevRTT); /* time in µs */
     
     /* Finally, use the formula for retransmission timeout interval as a function of EstimatedRTT and DevRTT */
-    packetdata->TimeoutInterval.tv_sec = rto_µs  / 1000000; /* convert µs to s */
-    packetdata->TimeoutInterval.tv_nsec = (rto_µs % 1000000) * 1000; /* convert µs to ns safely without overflowing */
+    pt->TimeoutInterval.tv_sec = rto_µs  / 1000000; /* convert µs to s */
+    pt->TimeoutInterval.tv_nsec = (rto_µs % 1000000) * 1000; /* convert µs to ns safely without overflowing */
 }
