c4cc2d6c7816c08b08f7ab78bd502dbe8a4360e5
SamuelJ11 <sjab225@uky.edu>
2026-05-09 03:29:59 +0000

    Added header files

---
+++ Project2/Task1.h
@@ -0,0 +1,12 @@
+#ifndef TASK1_H
+#define TASK1_H
+
+#include "TimeInterval.h"
+
+#define MAX_EVENTS 64
+#define MESSAGE_SIZE 16
+#define CLIENT_THREADS 4
+#define NUM_REQUESTS 1000000
+#define PIPELINE 4
+
+#endif
---
+++ Project2/Task2.h
@@ -0,0 +1,12 @@
+#ifndef TASK2_H
+#define TASK2_H
+
+#include "TimeInterval.h"
+#include "Packet.h"
+
+#define MAX_EVENTS 64
+#define CLIENT_THREADS 4
+#define NUM_REQUESTS 10000
+#define PIPELINE 4
+
+#endif
